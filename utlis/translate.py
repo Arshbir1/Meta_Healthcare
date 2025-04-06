09032659474dde0fc7af5b088da2b1cf2ed17407
Arshbir1 <55049279+Arshbir1@users.noreply.github.com>
2025-04-06 16:34:00 +0530

    Delete utlis/translate.py

--- utlis/translate.py
+++
@@ -1,4 +0,0 @@
-# Translation helper for loading and injecting strings
-# Loads selected language file (JSON)
-# Replaces placeholders in templates with translated strings
-# Can be used as Jinja filter or Flask context processor
