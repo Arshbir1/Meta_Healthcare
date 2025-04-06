025b0a36b826f6abe1a74db4a84b11a7e0b8a291
Arshbir1 <55049279+Arshbir1@users.noreply.github.com>
2025-04-06 16:14:34 +0530

    Create translate.py

---
+++ utlis/translate.py
@@ -0,0 +1,4 @@
+# Translation helper for loading and injecting strings
+# Loads selected language file (JSON)
+# Replaces placeholders in templates with translated strings
+# Can be used as Jinja filter or Flask context processor
